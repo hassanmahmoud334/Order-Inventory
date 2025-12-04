153c6fcd2fc2021b1c3aeeaea966a458b0eea098
hassanmahmoud334 <hassanmahmoud334@gmail.com>
2025-12-04 02:27:15 +0200

    Add project files.

---
+++ Order Service/Program.cs
@@ -0,0 +1,13 @@
+using Order_Service.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<OrderService>();
+app.MapGet("/", () => "Order Service running - use gRPC client.");
+
+app.Run();
---
+++ Order Service/Services/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace Order_Service.Services
+{
+    public class OrderService(ILogger<OrderService> logger) : PlaceOrder.PlaceOrderBase
+    {
+        public override Task<PlaceOrderResponse> PlaceOrder(PlaceOrderRequest request, ServerCallContext context)
+        {
+            logger.LogInformation("Processing order for ProductId: {ProductId}, Quantity: {Quantity}", request.ProductId, request.Quantity);
+
+            var response = new PlaceOrderResponse
+            {
+                OrderId = Guid.NewGuid().ToString(),
+                Success = true,
+                Message = "Your order has been placed."
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
